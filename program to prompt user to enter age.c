e9a8acc9caff785108fedbdeeb1d4fdede185272
David2006mwita <davidmwita2006@gmail.com>
2025-09-19 14:40:40 +0300

    Add files via upload

---
+++ program to prompt user to enter age.c
@@ -0,0 +1,15 @@
+/*
+
+Name:David Mwita
+Registration No:CT101/G/26419/25
+Description : Program to prompt the user to enter age
+
+*/
+#include<stdio.h>
+int main(){
+int age;
+printf("enter your age :\n");
+scanf("%d,&age");
+printf("your age is %d,age");
+return 0;
+}
