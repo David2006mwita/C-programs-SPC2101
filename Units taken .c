94ed57acc4569db1c47f75665f59e30ce0f05562
David2006mwita <davidmwita2006@gmail.com>
2025-09-19 14:09:31 +0300

    Add files via upload

---
+++ Units taken .c
@@ -0,0 +1,19 @@
+/*
+
+Name: David Mwita
+Registration no:CT101/G/26419/25
+Description:Units taken
+
+*/
+#include<stdio.h>
+int main(){
+printf("Introduction to programming and problem solving \n");
+printf("physics \n");
+printf("Analogue electronics \n");
+printf("Communication skills \n");
+printf("discrete mathematics \n");
+printf("fundamentals of ICT \n");
+printf("Mathematics for science \n");
+printf("Introduction to computer architecture");
+return 0;
+}
